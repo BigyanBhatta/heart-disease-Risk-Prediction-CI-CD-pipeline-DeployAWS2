0150416cd8310ba25abc5701f71803e945f77e34
Bigyan <bigyanbhatta1193@gmail.com>
2023-12-19 14:19:20 +0545

    exception.py and logger.py added

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements(file_path: str) -> List[str]:
     return requirements
 
 setup(
-    name = 'classification_project',
+    name = 'heart_disease_Project',
     version = '0.0.1',
     author = 'Bigyan',
     author_email = 'bigyanbhatta1193@gmail.com',
